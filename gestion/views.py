1399f59865ed9334c0db7428d162e08979c8b7c1
NBS-BADIE <badienbs@gmail.com>
2025-11-25 16:47:40 +0100

    Update views.py

--- gestion/views.py
+++ gestion/views.py
@@ -27,6 +27,16 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import os, json
 
+# gestion/views.py
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def apply_migrations(request):
+    call_command('migrate')
+    return HttpResponse("Migrations applied!")
+
+
+
 
 # =================== Compteur global ===================
 def get_compteur(request):
